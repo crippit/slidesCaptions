571c4e75e1ef85ec07d278711484eb4bc3464ec9
crippit <crippit@gmail.com>
2022-01-15 16:56:02 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,28 @@
+document.addEventListener("DOMContentLoaded", documentEvents, false);
 
+function documentEvents() {
+  const clearBtn = document.getElementById("clear");
+  const submitBtn = document.getElementById("submit");
+  const inputTxt = document.getElementById("imgUrlInput");
+
+  submitBtn.addEventListener("click", () => {
+   
+    });
+
+   
+
+  clearBtn.addEventListener("click", () => {
+    });
+}
+
+function convertIntoCss(url) {
+  const css = `html body { 
+        background: url(${url}); \n
+        image-rendering: crisp-edges; \n        
+        image-rendering: -webkit-optimize-contrast; \n
+        background-size:     cover; \n
+        background-repeat:   no-repeat; \n
+        background-position: center center; \n    
+      }\n`;
+  return css;
+}
