cc0cd31b870c77e30f395e15a59dfa0377248d6b
crippit <crippit@gmail.com>
2022-01-17 21:39:41 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -7,7 +7,9 @@ function changeSize() {
   const submitBtn = document.getElementById("submit");
   var textSize = document.getElementById("size").value;
   document.getElementById("sizeOutput").innerHTML = textSize;
-
+  chrome.storage.local.set({key: textSize}, function() {
+  console.log('Value is set to ' + textSize);
+});
   submitBtn.addEventListener("click", () => {
    
     });
@@ -16,16 +18,4 @@ function changeSize() {
 
   clearBtn.addEventListener("click", () => {
     });
-}
-
-function convertIntoCss(url) {
-  const css = `html body { 
-        background: url(${url}); \n
-        image-rendering: crisp-edges; \n        
-        image-rendering: -webkit-optimize-contrast; \n
-        background-size:     cover; \n
-        background-repeat:   no-repeat; \n
-        background-position: center center; \n    
-      }\n`;
-  return css;
 }
