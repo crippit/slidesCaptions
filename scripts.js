1e02cdfdc5a1a52af1b38f44d92fa58a67b98e07
crippit <crippit@gmail.com>
2022-01-17 21:37:57 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
-const css = 'body { background-color: red; }';
+function injectCSS() {
+
+    chrome.storage.local.get(['size'], function(result) {
+        console.log('Value currently is ' + result.size);
+      });
+const css = '.Mz6pEf {font-size: '+chosenSize+'pt !important;}';
 const tabId = getTabId();
 chrome.scripting.insertCSS(
     {
@@ -6,3 +11,5 @@ chrome.scripting.insertCSS(
       css: css,
     },
     () => { ... });
+          
+}
