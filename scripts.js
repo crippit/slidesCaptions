3394d12e728014d0fb23bd333e7ab504d82efd93
James <ptrsnja@gmail.com>
2022-03-17 19:45:10 -0400

    toggle on and off by clicking extension icon

--- scripts.js
+++ scripts.js
@@ -1,7 +1,19 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.insertCSS(
-    {
-      target: {tabId: tab.id},
-      files: ["./styles.css"],
-    },);
-})
+chrome.action.onClicked.addListener(async (tab) => {
+  console.log('tab', tab);
+  const title = await chrome.action.getTitle({tabId: tab.id});
+  if (title === 'Click to enable large captions') {
+    chrome.scripting.insertCSS(
+      {
+        target: {tabId: tab.id},
+        files: ["./styles.css"],
+      });
+    chrome.action.setTitle({tabId: tab.id, title: 'Large captions are enabled'});
+  } else {
+    chrome.scripting.removeCSS(
+      {
+        target: {tabId: tab.id},
+        files: ["./styles.css"],
+      });
+    chrome.action.setTitle({tabId: tab.id, title: 'Click to enable large captions'});
+  }
+});
