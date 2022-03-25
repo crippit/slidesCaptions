17274eaab7ff50a7941157d7dcd19b7229531e65
Chris <cwebb02@lbpearson.ca>
2022-03-24 20:36:09 -0400

    not working

--- options.js
+++ options.js
@@ -1,35 +1,15 @@
-function save_options() {
-  var fontSize = document.getElementById('font-size').value;
-  var fontColor = document.getElementById('font-color').value;
-  var fontType = document.getElementById('font').value;
-  chrome.storage.sync.set({
-    fontSize: fontSize,
-    fontColor: fontColor,
-    fontType: fontType
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
-}
+const options = {};
+
+optionsForm.fontSize.addEventListener('change', (event) => {
+  options.fontSize = event.target.value;
+  chrome.storage.sync.set({options});
+});
+
+
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
-function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    fontColor: 'white',
-    fontSize: 'medium',
-    fontType: 'Sans-serif'
-  }, function(items) {
-    document.getElementById('font-color').value = items.fontColor;
-    document.getElementById('font-size').value = items.fontSize;
-    document.getElementById('font').value = items.fontType;
-  });
-}
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+chrome.storage.sync.get('options', (data) => {
+    Object.assign(options, data.options);
+    optionsForm.fontSize.value = options.fontSize;;
+});
